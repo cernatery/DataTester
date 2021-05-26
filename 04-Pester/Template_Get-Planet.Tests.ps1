beaac4fb6e807ef487eca49ff2e4d1acfcdcc0b2
Tereza Cerna <cernatery@gmail.com>
2021-05-26 13:15:25 +0200

    Update Template_Get-Planet.Tests.ps1

--- 04-Pester/Template_Get-Planet.Tests.ps1
+++ 04-Pester/Template_Get-Planet.Tests.ps1
@@ -1,44 +1,45 @@
-# BeforeAll defines a series of steps to perform at the beginning of the current container, Context or Describe block.
+# V části 'BeforeAll' definujeme kroky, které se mají provést před samotným testem jako příprava na testování.
 BeforeAll { 
+    # Definujeme funkci Get-Planet, která obsahuje názvy všech planet naší sluneční soustavy.
     function Get-Planet ([string]$Name = '*') {
         $planets = @(
-            @{ Name = 'Mercury' }
-            @{ Name = 'Venus'   }
-            @{ Name = 'Earth'   }
+            @{ Name = 'Merkur' }
+            @{ Name = 'Venuse'   }
+            @{ Name = 'Zeme'   }
             @{ Name = 'Mars'    }
             @{ Name = 'Jupiter' }
             @{ Name = 'Saturn'  }
-            @{ Name = 'Uranus'  }
-            @{ Name = 'Neptune' }
+            @{ Name = 'Uran'  }
+            @{ Name = 'Neptun' }
         ) | ForEach-Object { [PSCustomObject] $_ }
 
         $planets | Where-Object { $_.Name -like $Name }
     }
 }
 
-# Describe creates a logical group of tests. A Describe block may contain any number of Context and It blocks.
+# Blok 'Describe' slouží jako Test Case, tvoří logickou sadu testů.
 Describe 'Get-Planet' {
-# It validates the results of a test inside of a Describe block. The body of the It block is the appropriate location for an assert.
-    It 'Given no parameters, it lists all 8 planets' {
+# Blok 'It' je samotný test.
+    It 'Funkce obsahuje 8 planet' {
         $allPlanets = Get-Planet
         $allPlanets.Count | Should -Be 8
     }
-    It 'Earth is the third planet in our Solar System' {
+    It 'Země je třetí planetou v seznamu' {
         $allPlanets = Get-Planet
-        $allPlanets[2].Name | Should -Be 'Earth'
+        $allPlanets[2].Name | Should -Be 'Zeme'
     }
-    It 'Pluto is not part of our Solar System' {
+    It 'Seznam planet neobsahuje Pluto' {
         $allPlanets = Get-Planet
         $plutos = $allPlanets | Where-Object Name -EQ 'Pluto'
         $plutos.Count | Should -Be 0
     }
-    It 'Planets have this order: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune' {
+    It 'Funkce vrací planety v tomto pořadí: Merkur, Venuse, Zeme, Mars, Jupiter, Saturn, Uran, Neptun' {
         $allPlanets = Get-Planet
         $planetsInOrder = $allPlanets.Name -join ', ' 
-        $planetsInOrder | Should -Be 'Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune'
+        $planetsInOrder | Should -Be 'Merkur, Venuse, Zeme, Mars, Jupiter, Saturn, Uran, Neptun'
     } 
 
-    # AfterAll: The typical usage is to clean up state or temporary used in tests.
+    # V bloku 'AfterAll' se mažou všechny dočasné změny vytvořené za účelem testování.
     AfterAll {
 
     }
